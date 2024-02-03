9866b6c10e57db248eb5a0ec8ca212efdabbfe48
Harsh Vardhan Sharma <harshvsharma1804@gmail.com>
2024-02-04 01:18:21 +0530

    primary setup initiated

---
+++ backend/src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "your_db_name";
---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants'
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDB Connection Failed!!",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
